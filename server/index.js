82e7f80a3c1fcb34be457a2019ce9470e1ff3de0
Simas <simonas@digitouch.lt>
2019-05-30 14:58:54 +0300

    setup basic express server and router

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const volleyball = require('volleyball');
+const router = require('./auth');
+
+const app = express();
+app.use(volleyball);
+app.use(router);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Ahoy Sailor o/ ⛵',
+  });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
