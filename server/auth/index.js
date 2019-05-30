df6cd5e771959158d7ffd36b54ea363664c6ee09
Simas <simonas@digitouch.lt>
2019-05-30 16:34:04 +0300

    add db connection

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const Joi = require('joi');
+
+const db = require('../db/conn');
+const users = db.get('users');
+users.createIndex('username', { unique: true });
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const schema = Joi.object().keys({
+  username: Joi.string()
+    .regex(/(^[a-zA-Z0-9_-]+$)/)
+    .min(3)
+    .max(30)
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .max(20)
+    .required(),
+});
+
+router.get('/', (req, res) => {
   res.json({
     msg: 'Route Operational o/ 🚗',
   });
+});
+
+router.post('/signup', (req, res) => {
+  console.log('body', req.body);
+  const result = Joi.validate(req.body, schema);
+  res.json(result);
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const volleyball = require('volleyball');
-const router = require('./auth');
+
+const auth = require('./auth');
 
 const app = express();
+
 app.use(volleyball);
-app.use(router);
+app.use(express.json());
+
+app.use('/auth', auth);
 
 app.get('/', (req, res) => {
   res.json({
