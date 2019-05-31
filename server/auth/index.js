fb1fa9a7b7261dbddeb03df46cbc9c3b663301e0
Simas <simonas@digitouch.lt>
2019-05-31 09:20:59 +0300

    add user signup functionality, pass hash and db check

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 
 const db = require('../db/conn');
+db.then(() => console.log('Connected to the database')).catch(error => console.log(error));
 const users = db.get('users');
 users.createIndex('username', { unique: true });
 
@@ -14,6 +16,7 @@ const schema = Joi.object().keys({
     .max(30)
     .required(),
   password: Joi.string()
+    .trim()
     .min(6)
     .max(20)
     .required(),
@@ -25,10 +28,42 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/signup', (req, res) => {
-  console.log('body', req.body);
+router.post('/signup', (req, res, next) => {
   const result = Joi.validate(req.body, schema);
-  res.json(result);
+  const { username, password } = req.body;
+  // If no errors, check for user in DB
+  if (result.error === null) {
+    users
+      .findOne({
+        username,
+      })
+      .then(user => {
+        // If no user found, hash the password, else, throw error
+        if (user) {
+          const error = new Error('User already exists');
+          next(error);
+        } else {
+          bcrypt
+            .hash(password, 12)
+            .then(hashedPass => {
+              // After pasword hashing, insert user to db
+              const newUser = {
+                username,
+                password: hashedPass,
+              };
+              users.insert(newUser).then(user => {
+                delete user.password;
+                res.json(user);
+              });
+            })
+            .catch(error => {
+              next(error);
+            });
+        }
+      });
+  } else {
+    next(result.error);
+  }
 });
 
 module.exports = router;
